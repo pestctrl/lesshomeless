b77991700a88a378b5ff9c9368cf5b8db44c95c5
Benson Chu <bensonchu457@gmail.com>
2019-01-25 23:44:29 -0600

    Basic game and day manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public DayManager daym;
+    int money;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        money = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!daym.active) {
+            Debug.Log("Hello, new day!");
+            daym.BeginDay();
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    public void WrapUpDay() {
+        Debug.Log("The day is over!!");
+        money++;
+        Debug.Log("You have $" + money + "!!!!");
+        this.gameObject.SetActive(true);
     }
 }

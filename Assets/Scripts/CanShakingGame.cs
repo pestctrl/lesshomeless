fda9597d6924cba5758c286d333ad052142e51f3
xj4966 <jkbroaddus100@gmail.com>
2019-01-25 23:55:34 -0600

    CanShakingMVP

---
+++ Assets/Scripts/CanShakingGame.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanShakingGame : MonoBehaviour
+{
+    public GameObject Can;
+    public float coinMultiplier;
+    public bool coinGameStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (coinGameStart)
+        {
+            coinMultiplier = GetMultiplier();
+        }
+    }
+
+    float GetMultiplier()
+    {
+        float mult;
+        Vector3 lastVelocity = Can.GetComponent<Rigidbody>().velocity;
+
+        mult = lastVelocity.magnitude * 10;
+        return mult;
+
+    }
+}

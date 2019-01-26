445b69b6728f2a682b4ae2a7ac4a1b2218289a20
xj4966 <jkbroaddus100@gmail.com>
2019-01-26 14:10:05 -0600

    Shop stuff more testing I guess

--- Assets/Scripts/Manaagers/ShopManager.cs
+++ Assets/Scripts/Manaagers/ShopManager.cs
@@ -34,29 +34,33 @@ public class ShopManager : MonoBehaviour
                 break;
             case "SwordFish_ShopItem":
                 itemPrice = 0;
-                if (GM.money > itemPrice)
+                if (GM.money >= itemPrice)
                 {
+                    print("added fish");
                     IM.haveSwordfish = true;
                 }
                 break;
             case "DogFood_ShopItem":
                 itemPrice = 0;
-                if (GM.money > itemPrice)
+                if (GM.money >= itemPrice)
                 {
+                    print("added dogfood");
                     IM.numDogfoods++;
                 }
                 break;
             case "Water_ShopItem":
                 itemPrice = 0;
-                if (GM.money > itemPrice)
+                if (GM.money >= itemPrice)
                 {
+                    print("added water");
                     IM.waterVal = IM.waterVal+25;
                 }
                 break;
             case "Food_ShopItem":
                 itemPrice = 0;
-                if (GM.money > itemPrice)
+                if (GM.money >= itemPrice)
                 {
+                    print("added food");
                     IM.foodVal = IM.foodVal+25;
                 }
                 break;

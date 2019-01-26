b43c5bab92eda4ebd0a718d14db07957f652b095
Benson Chu <bensonchu457@gmail.com>
2019-01-26 11:33:49 -0600

    Separated sun from day script

--- Assets/Scripts/DayManager.cs
+++ Assets/Scripts/DayManager.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class DayManager : MonoBehaviour
 {
-    public Light sun;
-
     public GameObject environment;
 
     public GameManager gmparent;
@@ -13,44 +11,39 @@ public class DayManager : MonoBehaviour
     public CanShakingGame can;
     public SqueegeeGame squeegee;
 
+    public SunTimer sun;
+
     public HomelessGame activeGame;
     
-    public float secondsInFullDay = 20f;
-    [HideInInspector]
-    public float timeMultiplier = 1f;
-    float sunInitialIntensity;
-    public float time;
     public bool gameSelected;
     public bool active;
     
     void Start()
     {
         active = false;
-        sunInitialIntensity = sun.intensity;
     }
 
     public void BeginDay(InventoryManager inven)
     {
-        // Reset timer, and show the environment
-        time = 0.15f;
+        // Show the environment
         environment.SetActive(true);
 
         // Let the gamemanager know that the day is on it's way
         active = true;
-
-        gameSelected = false;
 
         // Only show these items if purchased
         squeegee.gameObject.SetActive(inven.haveSqueegee);
 
         // Wait for a game to be selected
         this.gameObject.SetActive(false);
+        gameSelected = false;
     }
 
     public void GameSelected(string tag)
     {
         gameSelected = true;
         Debug.Log("The Game has been selected " + tag);
+        sun.StartDayTimer();
 
         // Update the day counter and sun cycle
         this.gameObject.SetActive(true);
@@ -67,19 +60,14 @@ public class DayManager : MonoBehaviour
     void Update()
     {
         if(active) {
-            // Move time forward 
-            time += (Time.deltaTime / secondsInFullDay) * timeMultiplier;
-            UpdateSun();
-            
-            if(time > 1) {
-                Debug.Log("Day is done");
-
+            if(!sun.active) {
                 Debug.Log("Earned $" + activeGame.GetMoney());
                 // Grab money generated
                 gmparent.AddMoney(activeGame.GetMoney());
 
                 // Disable all games
                 can.enabled = false;
+                squeegee.enabled = false;
                 
                 // Finish Day
                 active = false;
@@ -91,22 +79,5 @@ public class DayManager : MonoBehaviour
     void EndDay() {
         environment.SetActive(false);
         gmparent.GoToBox();
-    }
-
-    void UpdateSun() {
-        sun.transform.localRotation = Quaternion.Euler((time * 360f) - 90, 170, 0);
- 
-        float intensityMultiplier = 1;
-        if (time <= 0.23f || time >= 0.75f) {
-            intensityMultiplier = 0;
-        }
-        else if (time <= 0.25f) {
-            intensityMultiplier = Mathf.Clamp01((time - 0.23f) * (1 / 0.02f));
-        }
-        else if (time >= 0.73f) {
-            intensityMultiplier = Mathf.Clamp01(1 - ((time - 0.73f) * (1 / 0.02f)));
-        }
- 
-        sun.intensity = sunInitialIntensity * intensityMultiplier;
     }
 }
---
+++ Assets/Scripts/SunTimer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunTimer : MonoBehaviour
+{
+    public Light sun;
+    public float secondsInFullDay = 20f;
+    [HideInInspector]
+    public float timeMultiplier = 1f;
+    float sunInitialIntensity;
+    public float time;
+    public bool active;
+    
+    void Start()
+    {
+        active = false;
+        sunInitialIntensity = sun.intensity;
+    }
+    
+    public void StartDayTimer()
+    {
+        time = 0.18f;
+        active = true;
+    }
+
+    void Update()
+    {
+        if(active) {
+            time += (Time.deltaTime / secondsInFullDay) * timeMultiplier;
+            UpdateSun();
+            
+            if(time > 1) {
+                Debug.Log("Day is done");
+
+                active = false;
+            }
+        }
+    }
+
+    void UpdateSun() {
+        sun.transform.localRotation = Quaternion.Euler((time * 360f) - 90, 170, 0);
+ 
+        float intensityMultiplier = 1;
+        if (time <= 0.23f || time >= 0.75f) {
+            intensityMultiplier = 0;
+        }
+        else if (time <= 0.25f) {
+            intensityMultiplier = Mathf.Clamp01((time - 0.23f) * (1 / 0.02f));
+        }
+        else if (time >= 0.73f) {
+            intensityMultiplier = Mathf.Clamp01(1 - ((time - 0.73f) * (1 / 0.02f)));
+        }
+ 
+        sun.intensity = sunInitialIntensity * intensityMultiplier;
+    }
+}

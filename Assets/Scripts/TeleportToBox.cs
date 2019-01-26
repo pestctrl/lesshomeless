31fd7efdbc382874afb6e571a708d5dcd7516a13
xj4966 <jkbroaddus100@gmail.com>
2019-01-26 14:56:35 -0600

    TeleportToBox script to call MoveToBox

---
+++ Assets/Scripts/TeleportToBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportToBox : MonoBehaviour
+{
+    public GameManager GM;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Box")
+        {
+            GM.GoToBox();
+        }
+    }
+}

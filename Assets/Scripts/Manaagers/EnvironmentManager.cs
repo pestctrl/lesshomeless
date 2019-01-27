b66c312ed537a11c8c655283d7b9488423d832d7
Benson Chu <bensonchu457@gmail.com>
2019-01-27 13:07:47 -0600

    Randomized cars

--- Assets/Scripts/Manaagers/EnvironmentManager.cs
+++ Assets/Scripts/Manaagers/EnvironmentManager.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class EnvironmentManager : MonoBehaviour
 {
    // public GameObject[] CarSpawns = new GameObject[2];
-    public GameObject Car;
+    public GameObject[] CarTypes;
     public GameObject Person;
+    int delayLowerBound = 2;
+    int delayUpperBound = 6;
+    int timeDestroy = 10;
+    float thrust = 120f; 
     // Start is called before the first frame update
     void OnEnable()
     {
-        StartCoroutine(BackAndFourthCycle(GameObject.FindGameObjectsWithTag("CarSpawn")[0], Car, 120, 10, Random.Range(4, 7)));
+        delayLowerBound = 2;
+        delayUpperBound = 6;
+        StartCoroutine(BackAndFourthCycle(GameObject.FindGameObjectsWithTag("CarSpawn")[0]));
 
     }
 
@@ -20,18 +26,19 @@ public class EnvironmentManager : MonoBehaviour
        
     }
 
-    IEnumerator BackAndFourthCycle(GameObject spawn, GameObject obj, float thrust, float timeDestroy, float timeSpawnDelay)
+    IEnumerator BackAndFourthCycle(GameObject spawn)
     {
             bool spawned = false;
             while (!spawned)
             {
+                GameObject obj = CarTypes[(int)Random.Range(0,3)];
                 GameObject instance = Instantiate(obj, spawn.transform.position, spawn.transform.rotation);
                 spawned = true;
                 //randomize color of instance
                 //randomize type of car
                 instance.GetComponent<Rigidbody>().AddForce(-1 * obj.transform.up * thrust, ForceMode.Force);
                 StartCoroutine(DestroyObjDelay(instance, timeDestroy));
-                yield return new WaitForSeconds(timeSpawnDelay);
+                yield return new WaitForSeconds(Random.Range(2, 6));
                 spawned = false;
             }  
     }
